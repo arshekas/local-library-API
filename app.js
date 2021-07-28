4de5558e4a23b79c5e7435452a70501ffe51d3c8
Arshpreet Singh <asingh206@mylangara.ca>
2021-07-27 20:11:07 -0700

    Adding files and changes required for deployment

--- app.js
+++ app.js
@@ -16,7 +16,11 @@ var helmet = require('helmet');
 var app = express();
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://arshekas:Holiday-21@cluster0.wac6l.mongodb.net/local_library?retryWrites=true&w=majority';
+
+var dev_db_url = 'mongodb+srv://arshekas:Holiday-21@cluster0.wac6l.mongodb.net/local_library?retryWrites=true&w=majority'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+// var mongoDB = 'mongodb+srv://arshekas:Holiday-21@cluster0.wac6l.mongodb.net/local_library?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
